44e5661cd271dc764f43a123bbd6928ba3b134cd
Guido Stein <gstein@resurety.com>
2023-12-01 00:38:42 -0500

    📦 NEW: add image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
 	siteMetadata: {
-		title: `My Gatsby Site`,
-		siteUrl: `https://www.yourdomain.tld`,
+		title: "My First Gatsby Site",
 	},
 	plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
