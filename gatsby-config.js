187cb94b5956c4500231c3ffd089ffd13ed20a87
Guido Stein <gstein@resurety.com>
2023-11-30 23:14:52 -0500

    🐛 FIX: version control

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,19 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `My Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		"gatsby-plugin-mdx",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+	],
 };
